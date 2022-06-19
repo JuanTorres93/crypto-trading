e3307bbffd59ac4ee9e91197c4d0e067877e64ef
Juan Torres <juan93git@gmail.com>
2022-06-19 15:59:08 +0200

    CoingeckoMarketFinder provides markets to trade

---
+++ src/marketfinder.py
@@ -0,0 +1,239 @@
+from abc import ABC, abstractmethod
+from time import sleep
+
+import requests.exceptions
+from pycoingecko import CoinGeckoAPI
+
+
+class MarketFinder(ABC):
+    def __init__(self, api):
+        self.api = api
+        self._top_markets = []
+        self.markets_to_trade = []
+        self.pairs_for_exchange_vs_currency = []
+
+    @abstractmethod
+    def get_top_markets(self, vs_currency):
+        """
+        Retrieves all available information from the top markets
+        :param vs_currency: currency to complete the market
+        :return: list of dictionaries
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def list_top_symbols(self, vs_currency):
+        """
+        Should return an upper case list containing crypto symbols
+        :param vs_currency: currency to complete market
+        :return: list
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def _get_symbol_info(self, symbol, vs_currency):
+        raise NotImplementedError
+
+    @abstractmethod
+    def provide_markets_to_trade(self, exchange_id, vs_currency):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_pairs_for_exchange(self, exchange_id, vs_currency):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_pairs_for_exchange_vs_currency(self, exchange_id, vs_currency):
+        raise NotImplementedError
+
+
+class CoinGeckoMarketFinder(MarketFinder):
+    def __init__(self, api):
+        super().__init__(api)
+        if not isinstance(api, CoinGeckoAPI):
+            raise TypeError("api should be CoinGeckoAPI")
+
+        self.seconds_to_wait_on_http_error = 3
+
+    def _get_top_markets_unhandled(self, vs_currency, force=True):
+        if len(self._top_markets) == 0 or force:
+            markets = self.api.get_coins_markets(vs_currency)
+        else:
+            markets = self._top_markets
+
+        self._top_markets = markets
+        return markets
+
+    def get_top_markets(self, vs_currency, force=True):
+        markets_fetched = False
+
+        while not markets_fetched:
+            try:
+                markets = self._get_top_markets_unhandled(vs_currency,
+                                                          force=force)
+                markets_fetched = True
+            except requests.exceptions.HTTPError:
+                sleep(self.seconds_to_wait_on_http_error)
+
+        return markets
+
+    def list_top_symbols(self, vs_currency):
+        markets = self.get_top_markets(vs_currency)
+
+        symbols = list(
+            map(
+                lambda x: x['symbol'].upper(),
+                markets
+            )
+        )
+        return symbols
+
+    def _get_symbol_info(self, symbol, vs_currency):
+        markets = self.get_top_markets(vs_currency)
+        symbol_id = list(
+            filter(
+                lambda x: x['symbol'].upper() == symbol.upper(),
+                markets
+            )
+        )[0]['id']
+
+        symbol_info_fetched = False
+
+        while not symbol_info_fetched:
+            try:
+                symbol_info = self.api.get_coin_by_id(symbol_id)
+                symbol_info_fetched = True
+            except requests.exceptions.HTTPError:
+                sleep(self.seconds_to_wait_on_http_error)
+
+        symbol_info['symbol'] = symbol_info['symbol'].upper()
+        return symbol_info
+
+    def get_pairs_for_exchange(self, exchange_id, page: int = 1):
+        exchange_tickers_fetched = False
+
+        while not exchange_tickers_fetched:
+            try:
+                exchange_tickers = self.api.get_exchanges_tickers_by_id(
+                    exchange_id, page=page)
+                exchange_tickers_fetched = True
+            except requests.exceptions.HTTPError:
+                sleep(self.seconds_to_wait_on_http_error)
+
+        return exchange_tickers
+
+    def get_pairs_for_exchange_vs_currency(self, exchange_id, vs_currency):
+        # TODO ejecutarla al instacnciar y guardar resultados en una variable de instancia
+        # TODO EJECUTARLA CADA MES
+        tickers = []
+        page = 1
+
+        exchange_tickers = self.get_pairs_for_exchange(exchange_id, page=page)
+
+        while len(exchange_tickers['tickers']) > 0:
+            page += 1
+            for ticker in exchange_tickers['tickers']:
+                tickers.append(ticker)
+
+            exchange_tickers = self.get_pairs_for_exchange(exchange_id,
+                                                           page=page)
+
+        tickers = list(
+            filter(lambda x: x['target'].upper() == vs_currency.upper(),
+                   tickers)
+        )
+
+        tickers = list(
+            map(
+                lambda x: {
+                    'base': x['base'].upper(),
+                    'target': x['target'].upper(),
+                },
+                tickers
+            )
+        )
+
+        self.pairs_for_exchange_vs_currency = tickers
+        return tickers
+
+    def provide_markets_to_trade(self, exchange_id, vs_currency):
+        top_symbols = set(self.list_top_symbols(vs_currency))
+
+        if len(self.pairs_for_exchange_vs_currency) == 0:
+            symbols_in_exchange = self.get_pairs_for_exchange_vs_currency(
+                exchange_id, vs_currency)
+        else:
+            symbols_in_exchange = self.pairs_for_exchange_vs_currency
+
+        symbols_in_exchange = set(
+            map(
+                lambda x: x['base'],
+                symbols_in_exchange
+            )
+        )
+
+        symbols_to_fetch = top_symbols.intersection(symbols_in_exchange)
+
+        markets_to_trade = []
+
+        for symbol in symbols_to_fetch:
+            info = self._get_symbol_info(symbol, vs_currency)
+            lower_case_vs_currency = vs_currency.lower()
+
+            if lower_case_vs_currency in info['market_data'][
+                'price_change_percentage_1h_in_currency'] and \
+                    lower_case_vs_currency in info['market_data'][
+                'price_change_percentage_24h_in_currency'] and \
+                    lower_case_vs_currency in info['market_data'][
+                'price_change_percentage_7d_in_currency']:
+                markets_to_trade.append(
+                    {
+                        'symbol': symbol,
+                        'market_cap_rank': info['market_cap_rank'],
+                        'coingecko_rank': info['coingecko_rank'],
+                        'market_cap': info['market_data']['market_cap'],
+                        'high_24h': info['market_data']['high_24h'],
+                        'low_24h': info['market_data']['low_24h'],
+                        'price_change_percentage_1h': info['market_data'][
+                            'price_change_percentage_1h_in_currency'][
+                            lower_case_vs_currency],
+                        'price_change_percentage_24h': info['market_data'][
+                            'price_change_percentage_24h_in_currency'][
+                            lower_case_vs_currency],
+                        'price_change_percentage_7d': info['market_data'][
+                            'price_change_percentage_7d_in_currency'][
+                            lower_case_vs_currency],
+                    }
+                )
+
+        def filter_positive(ls, key):
+            filtered = list(
+                filter(
+                    lambda x: x[key] > 0,
+                    ls
+                )
+            )
+
+            return sorted(filtered, key=lambda x: x[key], reverse=True)
+
+        positive_7d = filter_positive(markets_to_trade, 'price_change_percentage_7d')
+        positive_24h = filter_positive(markets_to_trade, 'price_change_percentage_24h')
+        positive_1h = filter_positive(markets_to_trade, 'price_change_percentage_1h')
+
+        ordered_markets = positive_7d + positive_24h + positive_1h
+        markets_to_trade.clear()
+
+        if len(ordered_markets) > 0:
+            for market in ordered_markets:
+                if market not in markets_to_trade:
+                    markets_to_trade.append(market)
+
+        self.markets_to_trade.clear()
+        self.markets_to_trade = markets_to_trade
+        return markets_to_trade
+
+
+if __name__ == '__main__':
+    borrar = CoinGeckoMarketFinder(CoinGeckoAPI())
+    h = borrar.provide_markets_to_trade('binance', 'EUR')
+    pass
---
+++ tests/test_marketfinder.py
@@ -0,0 +1,76 @@
+from pycoingecko import CoinGeckoAPI
+import pytest
+
+import marketfinder
+
+
+coingecko_marketfinder = marketfinder.CoinGeckoMarketFinder(CoinGeckoAPI())
+
+
+def test_coingecko_gets_top_markets():
+    top_markets = coingecko_marketfinder.get_top_markets('EUR')
+
+    assert len(top_markets) == 100
+
+    actual_keys = set(top_markets[0].keys())
+    expected_keys = {'id', 'symbol', 'name', 'market_cap', 'high_24h', 'low_24h'}
+
+    assert expected_keys.issubset(actual_keys)
+
+
+def test_coingecko_lists_top_symbols():
+    top_symbols = coingecko_marketfinder.list_top_symbols('EUR')
+
+    assert len(top_symbols) == 100
+    assert type(top_symbols[0]) is str
+
+
+def test_coingecko_gets_detailed_market_information():
+    markets_info = coingecko_marketfinder._get_symbol_info('BTC', 'EUR')
+
+    actual_keys = set(markets_info.keys())
+    expected_keys = {'id', 'symbol', 'name', 'market_cap_rank', 'coingecko_rank',
+                     'market_data', 'community_data'}
+
+    assert expected_keys.issubset(actual_keys)
+
+    market_data_actual_keys = set(markets_info['market_data'].keys())
+    market_data_expected_keys = {'market_cap', 'high_24h', 'low_24h',
+                                 'price_change_percentage_1h_in_currency',
+                                 'price_change_percentage_24h_in_currency',
+                                 'price_change_percentage_7d_in_currency',
+                                 }
+
+    assert market_data_expected_keys.issubset(market_data_actual_keys)
+
+
+def test_coingecko_gets_pairs_for_exchange():
+    pairs = coingecko_marketfinder.get_pairs_for_exchange('binance')
+
+    assert type(pairs) is dict
+    assert set(['tickers']).issubset(set(pairs.keys()))
+
+
+def test_coingecko_gets_pairs_for_exchange_vs_currency():
+    pairs = coingecko_marketfinder.get_pairs_for_exchange_vs_currency('binance', 'EUR')
+
+    assert type(pairs) is list
+    assert type(pairs[0]) is dict
+
+    for pair in pairs:
+        assert pair['target'] == 'EUR'
+
+
+def test_coingecko_orders_markets_according_percentage_change():
+    markets_to_trade = coingecko_marketfinder.provide_markets_to_trade('binance', 'EUR')
+
+    assert type(markets_to_trade) is list
+    assert type(markets_to_trade[0]) is dict
+
+    expected_keys = {'symbol', 'market_cap_rank', 'coingecko_rank',
+                     'market_cap', 'high_24h', 'low_24h',
+                     'price_change_percentage_7d',
+                     'price_change_percentage_24h',
+                     'price_change_percentage_1h'}
+
+    assert expected_keys.issubset(set(markets_to_trade[0].keys()))
