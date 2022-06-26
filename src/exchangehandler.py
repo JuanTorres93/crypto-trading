d8a5efaf76052a11ed6f81610b829a108f9c5458
Juan Torres <juan93git@gmail.com>
2022-06-26 10:43:15 +0200

    CcxtExchangeHandler can fetch candles and make market buy orders

--- src/exchangehandler.py
+++ src/exchangehandler.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
 import ccxt
+import numpy as np
+import pandas as pd
 
 
 class ExchangeHandler(ABC):
@@ -8,7 +10,14 @@ class ExchangeHandler(ABC):
         self._exchange_api = exchange_api
 
     @abstractmethod
-    def buy_market_order(self):
+    def buy_market_order(self, symbol, vs_currency, amount):
+        """
+        Buys in the market symbol vs_currency the specified amount of symbol
+        :param symbol: symbol to buy
+        :param vs_currency: vs_currency to complete the amrket
+        :param amount: amount of symbol to buy
+        :return: filled order
+        """
         raise NotImplementedError
 
     @abstractmethod
@@ -16,8 +25,18 @@ class ExchangeHandler(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def get_candles(self, symbol, vs_currency, timeframe, num_candles,
-                    since=None):
+    def get_candles_for_strategy(self, symbol, vs_currency, timeframe, num_candles,
+                                 since=None):
+        """
+        Gets the last candles available. It ALWAYS returns one candle less than those
+        specified to avoid taking into account the current UNFINISHED candle
+        :param symbol: asset symbol
+        :param vs_currency: vs_currency symbol to complete market
+        :param timeframe: timeframe to fetch candles from 5m, 15m, 30m, 1h, ...
+        :param num_candles: total number of candles to fetch
+        :param since: since date to fetch candles
+        :return: pd.DataFrame
+        """
         raise NotImplementedError
 
     @abstractmethod
@@ -32,30 +51,115 @@ class CcxtExchangeHandler(ExchangeHandler):
 
         super().__init__(exchange_api)
 
-    def buy_market_order(self):
-        pass
+    def _amount_to_precision(self, symbol, vs_currency, amount):
+        """
+        NOT TESTED METHOD
+        :param symbol:
+        :param vs_currency:
+        :param amount:
+        :return:
+        """
+        market = f"{symbol}/{vs_currency}"
+        try:
+            amount = self._exchange_api.amount_to_precision(symbol=market,
+                                                            amount=amount)
+        except ccxt.errors.ExchangeError as e:
+            self._exchange_api.load_markets()
+            amount = self._exchange_api.amount_to_precision(symbol=market,
+                                                            amount=amount)
+
+        return amount
+
+    def buy_market_order(self, symbol, vs_currency, amount):
+        """
+        See description in parent class
+        """
+        market = f"{symbol}/{vs_currency}".upper()
+        # Fix amount to exchange standards not to raise exceptions
+        amount = self._amount_to_precision(symbol=symbol,
+                                           vs_currency=vs_currency,
+                                           amount=amount)
+
+        # Execute buy order
+        buy_order = self._exchange_api.create_order(symbol=market,
+                                                    type='market',
+                                                    side='buy',
+                                                    amount=amount)
+
+        order_info = {
+            "exchange_id": buy_order['id'],
+            "timestamp": buy_order['timestamp'],
+            "price": buy_order['price'],
+            "amount": buy_order['amount'],
+            "cost": buy_order['cost'],
+            "fee_in_asset": buy_order['fee']['cost'],
+        }
+
+        return order_info
 
     def sell_market_order(self):
         pass
 
-    def get_candles(self, symbol, vs_currency, timeframe, num_candles,
-                    since=None):
-        pass
+    def get_candles_for_strategy(self, symbol, vs_currency, timeframe, num_candles,
+                                 since=None):
+        """
+        See description in parent class
+        """
+        market = f"{symbol}/{vs_currency}".upper()
+
+        # Get candles open, high, low, close, volume information
+        candles_list = ccxt.binance().fetch_ohlcv(symbol=market, timeframe=timeframe,
+                                             limit=num_candles, since=since)
+
+        # Take all candles except for the current one (still not closed) and give
+        # them some columns names in the data frame
+        candles_df = pd.DataFrame(candles_list[:-1],
+                          columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
+
+        # Convert timestamp from Unix format to YYYY-MM-DD hh:mm:ss.sss format
+        candles_df['datetime'] = pd.to_datetime(candles_df['datetime'], unit='ms')
+        # Candle color
+        candles_df['is_green'] = candles_df['open'] < candles_df['close']
+        candles_df['is_red'] = candles_df['open'] > candles_df['close']
+        # Low values of the candle bodies
+        candles_df['candle_body_low'] = np.where(candles_df['is_green'] == True, candles_df['open'], candles_df['close'])
+        candles_df['candle_body_high'] = np.where(candles_df['is_green'] == True, candles_df['close'], candles_df['open'])
+
+        return candles_df
 
     def get_fees(self):
         pass
 
 
 class BinanceCcxtExchangeHandler(CcxtExchangeHandler):
-    def buy_market_order(self):
-        pass
+    pass
 
-    def sell_market_order(self):
-        pass
 
-    def get_candles(self, symbol, vs_currency, timeframe, num_candles,
-                    since=None):
-        pass
+if __name__ == '__main__':
+    import config
+    # import marketfinder as mf
+    # from pycoingecko import CoinGeckoAPI
+    #
+    # symbol = 'ada'
+    #
+    # bin_eh = BinanceCcxtExchangeHandler(ccxt.binance(
+    #     {
+    #         'apiKey': config.BINANCE_API_KEY,
+    #         'secret': config.BINANCE_SECRET_KEY,
+    #         'enableLimitRate': True,
+    #     }
+    # ))
+    #
+    # cg = mf.CoinGeckoMarketFinder(CoinGeckoAPI())
+    # markets = cg.get_top_markets('EUR')
+    # symbol_price = list(
+    #     filter(
+    #         lambda x: x['symbol'] == symbol,
+    #         markets
+    #     )
+    # )[0]['current_price']
+    #
+    # symbol_amount = 11 / symbol_price
+    #
+    # bin_eh.buy_market_order(symbol=symbol.upper(), vs_currency='EUR', amount=symbol_amount)
 
-    def get_fees(self):
-        pass
--- tests/test_exchangehandler.py
+++ tests/test_exchangehandler.py
@@ -1,13 +1,50 @@
 import pytest
 
 import ccxt
+import pandas as pd
+from pycoingecko import CoinGeckoAPI
 
+import config
 import exchangehandler as eh
+import marketfinder as mf
 
+
+# These variables are used to perform actual buys and sells
+# Sell value is slightly lower in order not to raise exceptions due to not
+# beign able to sell the bought amount since fees exists
+currency_to_test_real_trades = 'EUR'
+value_to_test_buys_real_trades = 11
+value_to_test_sells_real_trades = 10.8
 
 @pytest.fixture
 def binance_eh_no_keys():
     return eh.BinanceCcxtExchangeHandler(ccxt.binance())
+
+
+@pytest.fixture
+def bitcoin_price_eur():
+
+    cg = mf.CoinGeckoMarketFinder(CoinGeckoAPI())
+    markets = cg.get_top_markets('EUR')
+    btc_price = list(
+        filter(
+            lambda x: x['symbol'] == 'btc',
+            markets
+        )
+    )[0]['current_price']
+
+    return btc_price
+
+
+@pytest.fixture
+def binance_eh():
+    return eh.BinanceCcxtExchangeHandler(ccxt.binance(
+        {
+            'apiKey': config.BINANCE_API_KEY,
+            'secret': config.BINANCE_SECRET_KEY,
+            'enableLimitRate': True,
+        }
+    ))
 
 
 def test_ccxt_exchange_handler_raises_exception_on_init_with_wrong_api():
@@ -28,7 +65,29 @@ def test_ccxt_exchange_handler_not_raises_exception_on_init_with_right_api():
 
 
 def test_binance_ccxt_exchange_handler_gets_candles(binance_eh_no_keys):
-    # TODO añadir el parámetro since
-    candles = binance_eh_no_keys.get_candles(symbol='BTC', vs_currency='EUR',
-                                             timeframe='1h', num_candles='200')
+    candles = binance_eh_no_keys.get_candles_for_strategy(symbol='BTC',
+                                                          vs_currency='EUR',
+                                                          timeframe='1h',
+                                                          num_candles=20,
+                                                          since=None)
+
+    assert isinstance(candles, pd.DataFrame)
+    assert len(candles) == 19
+    assert set(candles.columns).issubset({'datetime', 'open', 'high', 'low', 'close',
+                                     'volume', 'is_green', 'is_red',
+                                     'candle_body_low', 'candle_body_high'})
+
+
+@pytest.mark.spends_money
+def test_binance_ccxt_exchange_handler_buys_at_market_price(binance_eh, bitcoin_price_eur):
+    amount = value_to_test_buys_real_trades / bitcoin_price_eur
+    order = binance_eh.buy_market_order(symbol='BTC', vs_currency=currency_to_test_real_trades,
+                                        amount=amount)
+
+    expected_keys = {'exchange_id', 'timestamp', 'price', 'amount', 'cost',
+                     'fee_in_asset'}
+
+    assert expected_keys.issubset(set(order.keys()))
+
+
 
