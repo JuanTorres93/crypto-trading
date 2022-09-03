18dc26420fd74a1b8a446e3c0d520011666f6693
Juan Torres <juan93git@gmail.com>
2022-09-03 15:48:34 +0200

    Modified sql queries.

--- sql_queries/all_results.sql
+++ sql_queries/all_results.sql
@@ -1,27 +1,24 @@
 SELECT
 	   status as "Result",
-	   symbol,
 	   count(status) as "Count",
 	   sum(vs_currency_result_no_fees) as "NO Commissions" ,
 	   sum(vs_currency_result_no_fees) - sum(entry_fee_vs_currency) - sum(exit_fee_vs_currency) as "WITH Commissions"
 FROM trade
-WHERE status IS NOT "opened"
-AND strategy_name IS "support_and_resistance_higher_timeframe_bullish_divergence"
---AND id <= 463
-AND id > 463
-GROUP BY --symbol, 
-status
+WHERE
+strategy_name IS "support_and_resistance_higher_timeframe_bullish_divergence"
+AND is_real IS TRUE
+--AND entry_date LIKE '02/09/2022 %:%:%'	-- CAMBIAR ABAJO TAMBIÉN 'DD/MM/YYYY %:%:%'
+GROUP BY status
 UNION ALL
 SELECT
 	"TOTAL",
-	"SYM",
 	count(status) as "Count",
 	sum(vs_currency_result_no_fees) as "NO Commissions",
 	sum(vs_currency_result_no_fees) - sum(entry_fee_vs_currency) - sum(exit_fee_vs_currency) as "WITH Commissions"
 FROM trade
-WHERE status IS NOT "opened"
-AND strategy_name IS "support_and_resistance_higher_timeframe_bullish_divergence"
---AND id <= 463
-AND id > 463
-
+WHERE 
+strategy_name IS "support_and_resistance_higher_timeframe_bullish_divergence"
+AND is_real IS TRUE
+--AND entry_date LIKE '02/09/2022 %:%:%'	-- CAMBIAR ARRIBA TAMBIÉN 'DD/MM/YYYY %:%:%'
+;
 -- hasta 463 con 100 barras para divergencia
--- sql_queries/create_control_view.sql
+++ sql_queries/create_control_view.sql
@@ -1,20 +1,20 @@
 CREATE VIEW IF NOT EXISTS control_view AS
 	SELECT
 		id,
-		symbol,
-		vs_currency_symbol,
+		symbol as "sym",
+		vs_currency_symbol as "vs_s",
 		entry_date,
+		exit_date,
 		COALESCE(modified_take_profit, take_profit) as "take profit",
-		entry_price,
+		entry_price as "entry price",
 		COALESCE(modified_stop_loss, stop_loss) as "stop loss",
 		status,
 		vs_currency_result_no_fees - entry_fee_vs_currency - exit_fee_vs_currency AS "trade result",
-		COALESCE(crypto_quantity_exit, crypto_quantity_entry) * (take_profit - entry_price) - entry_fee_vs_currency - exit_fee_vs_currency AS "trade if win COM",
-		COALESCE(crypto_quantity_exit, crypto_quantity_entry) * (stop_loss - entry_price) - entry_fee_vs_currency - exit_fee_vs_currency AS "trade if lose COM",
+		COALESCE(crypto_quantity_exit, crypto_quantity_entry) * (take_profit - entry_price) - entry_fee_vs_currency - COALESCE(exit_fee_vs_currency, entry_fee_vs_currency) AS "trade if win COM",
+		COALESCE(crypto_quantity_exit, crypto_quantity_entry) * (stop_loss - entry_price) - entry_fee_vs_currency - COALESCE(exit_fee_vs_currency, entry_fee_vs_currency) AS "trade if lose COM",
 		100 * abs(entry_price - stop_loss) / entry_price AS "risked percentage",
-		abs(take_profit - entry_price) / abs(entry_price - stop_loss) AS "risk reward ratio NO com",
-		-- "trade if win COM" / -"trade if lose COM"
-		(COALESCE(crypto_quantity_exit, crypto_quantity_entry) * (take_profit - entry_price) - entry_fee_vs_currency - exit_fee_vs_currency) / -(COALESCE(crypto_quantity_exit, crypto_quantity_entry) * (stop_loss - entry_price) - entry_fee_vs_currency - exit_fee_vs_currency) AS "risk reward ratio COM",
+		abs(take_profit - entry_price) / abs(entry_price - stop_loss) AS "RRR NO com",
+		(COALESCE(crypto_quantity_exit, crypto_quantity_entry) * (take_profit - entry_price) - entry_fee_vs_currency - exit_fee_vs_currency) / -(COALESCE(crypto_quantity_exit, crypto_quantity_entry) * (stop_loss - entry_price) - entry_fee_vs_currency - exit_fee_vs_currency) AS "RRR COM",
 		COALESCE(crypto_quantity_exit, crypto_quantity_entry) * entry_price AS "money on entry"
 	FROM trade
 	ORDER BY id DESC
