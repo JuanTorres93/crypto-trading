60caabcfe47887e11edbcd5803bba42f2e4550d5
Juan Torres <juan93git@gmail.com>
2023-02-22 16:39:46 +0100

    Added break even status to control view.

--- sql_queries/create_control_view.sql
+++ sql_queries/create_control_view.sql
@@ -1,5 +1,5 @@
 CREATE VIEW IF NOT EXISTS control_view AS
-	SELECT
+    SELECT
 		id,
 		symbol as "sym",
 		vs_currency_symbol as "vs_s",
@@ -8,7 +8,10 @@ CREATE VIEW IF NOT EXISTS control_view AS
 		COALESCE('(\/) ' || modified_take_profit || ' (\/)', take_profit) as "take profit",
 		entry_price as "entry price",
 		COALESCE('(/\) ' || modified_stop_loss || ' (/\)', stop_loss) as "stop loss",
-		status,
+		CASE
+			WHEN entry_price = modified_stop_loss AND (vs_currency_result_no_fees - entry_fee_vs_currency - exit_fee_vs_currency) < 0.1 THEN 'break even'
+			ELSE status
+		END as "status",
 		vs_currency_result_no_fees - entry_fee_vs_currency - exit_fee_vs_currency AS "trade result",
 		COALESCE(crypto_quantity_exit, crypto_quantity_entry) * (take_profit - entry_price) - entry_fee_vs_currency - COALESCE(exit_fee_vs_currency, entry_fee_vs_currency) AS "trade if win COM",
 		COALESCE(crypto_quantity_exit, crypto_quantity_entry) * (stop_loss - entry_price) - entry_fee_vs_currency - COALESCE(exit_fee_vs_currency, entry_fee_vs_currency) AS "trade if lose COM",
