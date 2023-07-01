a7648da856bb85d41c2f5c9a057057c7564bdf60
Juan Torres <juan93git@gmail.com>
2023-07-01 09:18:48 +0200

    Minor changes

--- sql_queries/cv_all_results.sql
+++ sql_queries/cv_all_results.sql
@@ -6,7 +6,7 @@ FROM control_view
 WHERE
 strategy_name LIKE "volume_ema_trading_strategy"
 AND is_real IS TRUE
---AND exit_date LIKE '%/06/2023 %:%:%'	-- CAMBIAR ABAJO TAMBIÉN 'DD/MM/YYYY %:%:%'
+--AND exit_date LIKE '%/07/2023 %:%:%'	-- CAMBIAR ABAJO TAMBIÉN 'DD/MM/YYYY %:%:%'
 GROUP BY status
 UNION ALL
 SELECT
@@ -17,5 +17,5 @@ FROM control_view
 WHERE 
 strategy_name LIKE "volume_ema_trading_strategy"
 AND is_real IS TRUE
---AND exit_date LIKE '%/06/2023 %:%:%'	-- CAMBIAR ARRIBA TAMBIÉN 'DD/MM/YYYY %:%:%'
+--AND exit_date LIKE '%/07/2023 %:%:%'	-- CAMBIAR ARRIBA TAMBIÉN 'DD/MM/YYYY %:%:%'
 ;
