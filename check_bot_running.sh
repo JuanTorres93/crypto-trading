91fc00d1f10f8ae1acfa000cf0117e42802ecc3d
Juan Torres <juan93git@gmail.com>
2023-07-04 19:28:58 +0200

    Added bash script to monitor bot execution.

---
+++ check_bot_running.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# The execution of this file is intented to be handled by cronie or similar
+
+folder="$HOME/logs_bot_failed"
+
+check_and_create_folder() {
+  if [ ! -d "$folder" ]; then
+    mkdir "$folder"
+    echo "The folder '$folder' has been created."
+  fi
+}
+
+if [[ ! $(pgrep -f "python services.py") ]]
+then
+    check_and_create_folder
+    mv "$HOME/botlog.log" "$folder/botlog - $(date)"
+    cd ~/crypto-trading/ && source venv/bin/activate && cd src/
+    python -c "import externalnotifier as en; en.externally_notify('El bot se ha parado. Se ha guardado el log y se va a intentar lanzarlo otra vez.')"
+    python services.py &
+fi
+
